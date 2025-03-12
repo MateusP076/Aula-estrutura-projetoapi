becc2bac5697060c173f756d970f2f2cf04a3187
aluno cmc <alunocmc@umc.br>
2025-03-12 10:38:11 -0300

    Criptografia

--- src/main/java/com/aula/ProjetoTeste/Carros/CarControler.java
+++ src/main/java/com/aula/ProjetoTeste/Carros/CarControler.java
@@ -1,15 +1,14 @@
 package com.aula.ProjetoTeste.Carros;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.aula.ProjetoTeste.Cursos.CursoRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/Carro")
@@ -28,8 +27,11 @@ public class CarControler {
         if (condicao!=null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Carro Ja existe");
         }   else {
+            var hashcarro= BCrypt.withDefaults().hashToString(12, carModels.getCarro().toCharArray());
+            carModels.setCarro(hashcarro);
             var carro = carRepository.save(carModels);
             return ResponseEntity.status(HttpStatus.CREATED).body(carro);
         }
+
     }
 }
--- src/main/java/com/aula/ProjetoTeste/User/UserControler.java
+++ src/main/java/com/aula/ProjetoTeste/User/UserControler.java
@@ -1,16 +1,15 @@
 package com.aula.ProjetoTeste.User;
 
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
@@ -25,14 +24,30 @@ public class UserControler {
     }
 
     @PostMapping ("/criar")
-    public ResponseEntity criar(@RequestBody UserModel userModel, HttpServletRequest request){
-            var criado=this.userRepository.findByUsername(userModel.getUsername());
-            if (criado!=null){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existente");
-            } else {
-                var salvar=this.userRepository.save(userModel);
+    public ResponseEntity criar(@RequestBody UserModel userModel, HttpServletRequest request) {
+        var criado = this.userRepository.findByUsername(userModel.getUsername());
+        if (criado != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existente");
+        } else {
+            var hahssenha= BCrypt.withDefaults().hashToString(12, userModel.getSenha().toCharArray());
+            userModel.setSenha(hahssenha);
+            var salvar = this.userRepository.save(userModel);
             return ResponseEntity.status(HttpStatus.CREATED).body(salvar);
-            }
+        }
+
     }
-    
+    @GetMapping("/Listar")
+    public List<UserModel> listar(){
+        List<UserModel> usuariocad = userRepository.findAll();
+        return usuariocad;
+    }
+    @PutMapping("/Atualizar")
+    public ResponseEntity atualizar(@RequestBody UserModel userModel) {
+        var criado= this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(criado);
+    }
+    @DeleteMapping("/Deletar/{iduser}")
+    public void deletar(@PathVariable UUID iduser) {
+        userRepository.deleteById(iduser);
+    }
 }
